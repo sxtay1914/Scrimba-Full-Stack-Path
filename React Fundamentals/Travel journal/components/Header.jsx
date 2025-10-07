0d8327184555b5deeaa0587cea6f472c65338035
sxtay1914 <167632565+sxtay1914@users.noreply.github.com>
2025-10-08 06:51:53 +0800

    Delete React Fundamentals/Travel journal directory

--- React Fundamentals/Travel journal/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header>
-            <img src="../images/globe.png" alt="globe icon" />
-            <h1>my travel journal.</h1>
-        </header>
-    )
-}
