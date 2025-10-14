a9d91a70e7e88f79f66818837cc17f964f11feb7
Jesmond Tay <jesmondtay1914@gmail.com>
2025-10-15 07:26:09 +0800

    Added new project

---
+++ React Fundamentals/Chef Claude/README.md
@@ -0,0 +1,6 @@
+# Lessons learnt
+1. Event Listeners
+1. State
+1. Conditional rendering
+1. Forms
+1. State management strats
---
+++ React Fundamentals/Chef Claude/components/ClaudeRecipe.jsx
@@ -0,0 +1,22 @@
+/**
+ * Challenge: See if you can figure out how to use the
+ * react-markdown package to render `props.recipe` as React
+ * elements rather than the plain markdown text.
+ * 
+ * Use the link in the slide to navigate directly to the
+ * package's instructions on how to use it.
+ * 
+ * Use the `suggested-recipe-container` class on the <section>
+ * below to get some free styling 🙂
+ */
+import ReactMarkdown from 'react-markdown'
+
+export default function ClaudeRecipe(props) {
+    return (
+        <section className="suggested-recipe-container">
+            <ReactMarkdown>
+                {props.recipe}
+            </ReactMarkdown>
+        </section>
+    )
+}
